1ee34a0e1125a9343fb7669c20922635446afd47
qtong204 <qmah0001@student.monash.edu>
2026-04-22 12:19:29 +0800

    update the class to ensure the LLM flw the structure

--- custom/custom_types.py
+++ custom/custom_types.py
@@ -1,19 +1,43 @@
-import pydantic
+from import BaseModel
+from typing import List
 
-class RAGChunkAndSrc(pydantic.BaseModel):
-    chunk: list[str]
-    source_id: str = None
-
-
-class RAGUpsertResult(pydantic.BaseModel):
-    ingested: int
+# class RAGChunkAndSrc(BaseModel):
+#     chunk: list[str]
+#     source_id: str = None
 
 
-class RAGSearchResult(pydantic.BaseModel):
-    contexts: list[str]
-    sources: list[str]
+# class RAGUpsertResult(BaseModel):
+#     ingested: int
 
-class RAGQueryRequest(pydantic.BaseModel):
-    answer: str
-    sources: list[str]
-    num_contexts: int
+
+# class RAGSearchResult(BaseModel):
+#     contexts: list[str]
+#     sources: list[str]
+
+# class RAGQueryRequest(BaseModel):
+#     answer: str
+#     sources: list[str]
+#     num_contexts: int
+
+
+
+class MatchResult(BaseModel):
+    fit_score: float
+    matching_skills: List[str]
+    missing_skills: List[str]
+    reason: str
+
+class JobSearchInfo(BaseModel):
+    job_id: str
+    title: str
+    company: str
+    job_description: str
+    location: str
+    employment_type: str
+    link: str
+
+class SummaryJobInfo(BaseModel):
+    brief_summary: str
+    hr_insights: str
+
+
